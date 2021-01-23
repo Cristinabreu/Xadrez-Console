123c00b50b345d04fb1ae9f127ad5ba001840ceb
Cristinabreu <cristinabreu2018@gmail.com>
2021-01-23 21:25:49 +0000

    Jogada especial roque pequeno e roque grande

--- Xadrez-Console/xadrez/Peao.cs
+++ Xadrez-Console/xadrez/Peao.cs
@@ -30,11 +30,11 @@ namespace xadrez {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos)) {
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
-                pos.definirValores(posicao.linha - 1, posicao.coluna +1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos)) {
+                pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
             }
@@ -48,11 +48,11 @@ namespace xadrez {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos)) {
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos)) {
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
                     mat[pos.linha, pos.coluna] = true;
                 }
 
